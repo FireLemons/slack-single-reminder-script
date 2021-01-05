aa8bffa8605accf9c0a2686020e6358730fc97de
firelemons <shenyang64@gmail.com>
2021-01-05 16:53:24 -0600

    example config logs script output

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,6 +3,6 @@ env :PATH, ENV['PATH']
 
 here = Dir.pwd
 
-every 1.day, at: '14:00' do
-  command "cd #{here} && ruby #{here}/remind.rb"
+every 1.day, at: '14:50' do
+  command "cd #{here} && ruby #{here}/remind.rb main formatted >> #{here}/log.txt 2>&1"
 end
