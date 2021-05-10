8663ea4093f24798a6412d0197c8b96780d8c431
firelemons <shenyang64@gmail.com>
2021-05-10 16:26:33 -0500

    refactored send message into function

--- remind.rb
+++ remind.rb
@@ -19,6 +19,22 @@ end
 
 previous_messages = File.file?('./previous_message_ids.json') ? JSON.parse(File.read('./previous_message_ids.json')) : {}
 
+def send_reminder (config, reminder)
+  send_message_request = Net::HTTP::Post.new(SEND_MESSAGE_URI, {
+    'Authorization': "Bearer #{config["token"]}",
+    'Content-Type': 'application/json'
+  })
+  send_message_request.set_form_data({
+    'channel': config['channel'],
+    'text': reminder
+  })
+  send_message_result = Net::HTTP.start(SEND_MESSAGE_URI.hostname, SEND_MESSAGE_URI.port, :use_ssl => true) do |http|
+    http.request(send_message_request)
+  end
+
+  return JSON.parse(send_message_result.body)['ts']
+end
+
 for message in ARGV
   # Delete the previous message if a previous message id was found
   if previous_messages.key?(message)
@@ -37,20 +53,7 @@ for message in ARGV
     end
   end
 
-  # Send reminder
-  send_message_request = Net::HTTP::Post.new(SEND_MESSAGE_URI, {
-    'Authorization': "Bearer #{config["token"]}",
-    'Content-Type': 'application/json'
-  })
-  send_message_request.set_form_data({
-    'channel': config['channel'],
-    'text': config['messages'][message]
-  })
-  send_message_result = Net::HTTP.start(SEND_MESSAGE_URI.hostname, SEND_MESSAGE_URI.port, :use_ssl => true) do |http|
-    http.request(send_message_request)
-  end
-
-  previous_messages[message] = JSON.parse(send_message_result.body)['ts']
+  previous_messages[message] = send_reminder(config, config['messages'][message])
 end
 
 File.write('./previous_message_ids.json', previous_messages.to_json)
